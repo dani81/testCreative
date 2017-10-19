8f3c547a2d5c01abaff666a0ac5560f89b0ba084
dani81 <31867232+dani81@users.noreply.github.com>
2017-10-19 10:51:26 -0600

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,42 +1,17 @@
-angular.module('News', ['ui.router'])
-  .config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-      $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: '/home.html',
-          controller: 'MainCtrl'
-      })
-        .state('posts', {
-        url: '/posts/{id}',
-        templateUrl: '/posts.html',
-        controller: 'PostCtrl'
-       });
-      $urlRouterProvider.otherwise('home');
-  }])
-  .factory('postFactory', [function(){
-    var o = {
-      posts: [
-      ]
-    };
-    return o;
-  }])
+angular.module('News', [])
+ 
   .controller('MainCtrl', [
     '$scope',
-    'postFactory',
-    function($scope, postFactory){
-      $scope.posts = postFactory.posts;
+    function($scope){
       $scope.addPost = function(){
         if($scope.formContent === '') { return; }
-        postFactory.posts.push({
+          $scope.posts.push({
           title: $scope.formContent,
           pokemonName: $scope.pokemon,
           picture: $scope.pokemon + ".png"
         });
+        console.log("here");
         console.log($scope.posts[0]);
-        $scope.posts = postFactory.posts;
         $scope.title = '';
       };
   }
