f47b42e8c6cd7d21383bddf66c38ebe9ec9017b0
dani81 <31867232+dani81@users.noreply.github.com>
2017-10-19 13:32:09 -0600

    Trying to get the JS to upload faster

--- pokemon.js
+++ pokemon.js
@@ -11,7 +11,7 @@ angular.module('pokemonBattle', [])
           pokemonName: $scope.pokemon,
           picture: $scope.pokemon + ".png"
         });
-       console.log("here");
+       console.log("here2");
        console.log($scope.posts[0]);
       };
   }
